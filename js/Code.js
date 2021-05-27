5c118340bc45d223b3e9dca46a995c57f7229eb6
Dhanya110 <73469914+Dhanya110@users.noreply.github.com>
2021-05-27 22:23:30 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,39 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "DATABASE";
+const accessCode3 = "FUNCTION";
+const accessCode4 = "ARRAY";
+const accessCode5 = "CLASS";
+
+function clues() {
+    
+    fill("white")
+    textSize(25)
+    text("R E V B A I L A", 50,190)
+    textSize(15)
+    text("Hint: Always changing, not constant !!", 30,220)
+// add code for displaying rest of the names and the hints.
+fill("white")
+textSize(25)
+text("A T A D A S B A", 430,190)
+textSize(16)
+text("Hint:Stores all information", 440,220)
+
+fill("white")
+textSize(25)
+text("C U T N I F O N", 780,190)
+textSize(16)
+text("Hint: Performs a particular task", 740,220)
+
+fill("white")
+textSize(25)
+text("Y A R A R ", 260,370)
+textSize(16)
+text("Hint: Consists of a collection of variables", 190,400)
+
+fill("white")
+textSize(25)
+text("S L A C S", 680,370)
+textSize(16)
+text("Hint: Defined structure to create an object ", 600,400)
+
+}
